764bc5c96369c42e055ed9403f1bf905fd3299c7
Sangyeob Lee <sy0414@gmail.com>
2016-07-29 10:53:12 +0900

    scroll problem fixed

--- static/js/script_mobile.js
+++ static/js/script_mobile.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$('div.btn1').click(function() {
-		$('html, body').animate({scrollTop: parseInt($('html').prop('scrollHeight') * 0.6232)}, 500);
+		$('.realbody').animate({scrollTop: parseInt($('.realbody').prop('scrollHeight') * 0.6232)}, 500);
 	});
 	$('div.btn2').click(function() {
 		window.open('/mobile/request_form/');
