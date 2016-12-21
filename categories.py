6db35193351d52613ab68d6e739dca7cb0f57c40
Sangyeob Lee <sy0414@gmail.com>
2016-12-22 08:25:39 +0900

    category unicoded

--- categories.py
+++ categories.py
@@ -1,13 +1,13 @@
 #-*-coding:utf-8-*-
 
 categories = {
-	"figure": ["피규어", "Figure"],
-	"pla": ["프라모델", "Pla-model"],
-	"arttoy": ["아트토이", "Arttoy"],
-	"diorama": ["디오라마", "Diorama"],
-	"babydoll": ["베이비돌", "Baby doll"],
-	"bjd": ["구체관절인형", "BJD"],
-	"drone": ["드론", "Drone"],
-	"diecast": ["다이캐스트", "Diecast"],
-	"rc": ["R/C모형", "Radio Control"]
+	"figure": [u"피규어", "Figure"],
+	"pla": [u"프라모델", "Pla-model"],
+	"arttoy": [u"아트토이", "Arttoy"],
+	"diorama": [u"디오라마", "Diorama"],
+	"babydoll": [u"베이비돌", "Baby doll"],
+	"bjd": [u"구체관절인형", "BJD"],
+	"drone": [u"드론", "Drone"],
+	"diecast": [u"다이캐스트", "Diecast"],
+	"rc": [u"R/C모형", "Radio Control"]
 }
